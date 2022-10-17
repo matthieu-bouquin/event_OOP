a26ffb2485cf15b6d3c3728bfb59cec451333965
matthieu BOUQUIN <matthieu.bouquin@ik.me>
2022-10-17 19:00:30 +0200

    first

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,53 @@
+require "pry"
+require "time"
+
 class Event
+    
+    attr_accessor :start_date, :duration, :title, :attendees
+    @@event_all = []
+
+
+    def initialize(event_start_date, event_duration, event_title, event_attendees)
+        @start_date = Time.parse(event_start_date)
+        @duration = event_duration
+        @title = event_title
+        @attendees = event_attendees
+        @@event_all << self
+    end
+    
+    def postdone_24h
+        @start_date = @start_date + 86400
+        return @start_date
+    end
+
+    def end_date
+        return @start_date + @duration
+    end
+
+    def is_past?
+        return @start_date < Time.now
+    end
+
+    def  is_future?
+        return @start_date > Time.now
+    end
+
+    def is_soon?
+        return @start_date - 1800 < Time.now
+    end
+
+    def to_s
+        puts ">Titre : #{@title}"
+        puts ">Date de début : #{@start_date.strftime("%Y-%m-%d %H:%M:%S")}"
+        puts ">Durée : #{@duration} minutes"
+        puts ">Invités : #{@attendees}"
+    end
+
 
 end
+
+
+
+
+binding.pry
+puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,35 @@
+require "pry"
+
 class User
 
+    attr_accessor :email, :age 
+    @@user_all = []
+
+
+    def initialize(email, age)
+        @email = email
+        @age = age
+        @@user_all << self
+    end
+
+
+    def self.all
+        return @@user_all
+    end
+
+    def self.find_by_email(email)
+        @@user_all.each do |user|
+            if user.email == email
+            return user
+            puts "Voici l'age du User trouvé : #{@age}"
+        end
+        end
+        
+    end
+
+
 end
+
+
+binding.pry
+puts "end of file"
